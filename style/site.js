cb90853172084663e6f385ce68e472b3977974bf
SplaIka <107166432+splaika135@users.noreply.github.com>
2022-10-13 14:27:17 +0900

    Update site.js

--- style/site.js
+++ style/site.js
@@ -1,1 +1,9 @@
+var data=[
+'a b c d e',
+'あ い う え お',
+'か き く け こ',
+'さ し す せ そ'
+],h='<table border="1">';
 
+for(var i=0;i<data.length; i++)h+=i?
+  '<tr><td>'+data[i].replace(/ /g,'</td><td>')+'</td></tr>':'<tr><th>'+data[i].replace(/ /g,'</th><th>')+'</th></tr>';document.write(h+'</table>');
