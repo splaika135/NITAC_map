775f102c6522745510d38fa518a6ee790eb23c6c
SplaIka <107166432+splaika135@users.noreply.github.com>
2022-11-24 13:22:45 +0900

    Update header-3.js

--- style/header-3.js
+++ style/header-3.js
@@ -10,18 +10,5 @@ navToggle.addEventListener("click", function () {
     navWrapper.classList.add("active");
     this.setAttribute("aria-label", "close menu");
     this.setAttribute("aria-expanded", "true");
-    searchForm.classList.remove("active");
   }
 });
-
-let searchToggle = document.querySelector(".search__toggle");
-let searchForm = document.querySelector(".search__form");
-
-searchToggle.addEventListener("click", showSearch);
-
-function showSearch() {
-  searchForm.classList.toggle("active");
-  navToggle.setAttribute("aria-expanded", "false");
-  navToggle.setAttribute("aria-label", "menu");
-  navWrapper.classList.remove("active");
-}
