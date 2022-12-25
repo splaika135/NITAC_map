f106d38acc678f0ee3575994e2dcc42542dcde93
SplaIka <107166432+splaika135@users.noreply.github.com>
2022-12-25 22:37:21 +0900

    Create news-full.js

---
+++ js/news-full.js
@@ -0,0 +1,43 @@
+window.onload = function(){
+
+let output = document.getElementById("table");
+getCsv("https://splaika135.github.io/NITAC_map/style/news.csv");
+
+function getCsv(data){
+	let xhr = new XMLHttpRequest(); 
+  xhr.open("GET",data,true);
+  xhr.onload = function (e) {
+		if (xhr.readyState === 4) {
+		  if (xhr.status === 200) {
+			  let responce = xhr.responseText;
+			  csvArr(responce);
+		  } else {
+      console.error(xhr.statusText);
+      }
+    }
+	};
+	xhr.send(null);
+}
+
+function csvArr(dataArr){
+	let arr =[];
+  let list = dataArr.split('\n');
+  for(let i = 0; i < list.length - 1; i++){
+    arr[i] = list[i].split(',');
+  }
+	htmlWrite(arr);
+}
+
+function htmlWrite(dataList){
+	let insert ="";
+  dataList.forEach(element => {
+    insert +='<tr>';
+    element.forEach((childElement) =>{
+      insert +=`<td>${childElement}</td>`;
+    });
+    insert +='</tr>'
+  });
+  output.innerHTML = insert;
+}
+
+}
