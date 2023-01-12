1df1941bfadd7de9d327f6dfd7f94b4b2156e9a6
SplaIka <107166432+splaika135@users.noreply.github.com>
2023-01-12 14:51:54 +0900

    Update includes.js

--- js/includes.js
+++ js/includes.js
@@ -10,7 +10,7 @@ window.onload = function(){
     '<link rel="stylesheet"　type="text/css" href="style/style.css">'+
     '<link rel="stylesheet" href="style/header-3.css">';
 
-    let head = document.getElementById('in-head'); // 追加する部分を取得
+    let head = document.getElementById('head'); // 追加する部分を取得
 
     head.insertAdjacentHTML('afterbegin', head_content);
     // insertAdjacentHTMLでHTMLを追加
@@ -19,13 +19,28 @@ window.onload = function(){
 
     // header
     let header_content = 
-    '<div id="header-inner">'+
-    '<h1>共通化デモサイト</h1>'+
-    '<ul>'+
-    '<li>TOP</li>'+
-    '<li>SERVICES</li>'+
-    '<li>CONTACT</li>'+
-    '</ul>'+
+    '<div class="wrapper site-header__wrapper">'+
+      '<div class="site-header__start">'+
+        '<a style="margin-left:15px" href="index.html" class="brand">明石高専マップ</a>'+
+      '</div>'+
+      '<div class="site-header__end">'+
+        '<nav class="nav">'+
+          '<button class="nav__toggle" aria-expanded="false" type="button">'+
+            '<img src="images/menu.png" width="30" height="28">'+
+          '</button>'+
+          '<ul class="nav__wrapper">'+
+            '<li class="nav__item"><a href="map/G.html">G科棟</a></li>'+
+            '<li class="nav__item"><a href="map/M.html">M科棟</a></li>'+
+            '<li class="nav__item"><a href="map/E.html">E科棟</a></li>'+
+            '<li class="nav__item"><a href="map/C.html">C科棟</a></li>'+
+            '<li class="nav__item"><a href="map/A.html">A科棟</a></li>'+
+            '<li class="nav__item"><a href="map/lib.html">情報センター</a></li>'+
+            '<li class="nav__item"><a href="map/eat.html">食堂</a></li>'+
+            '<!--  <li class="nav__item"><a href="route/route.html">ルート案内</a></li>  -->'+
+            '<li class="nav__item"><a href="images/images.html">マップ画像集</a></li>'+
+          '</ul>'+
+        '</nav>'+
+      '</div>'+
     '</div>';
 
     let header = document.getElementById('header'); // 追加する部分を取得
