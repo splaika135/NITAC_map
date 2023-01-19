a4893a42d39dbaa8014def7661c01aed0a121c7c
SplaIka <107166432+splaika135@users.noreply.github.com>
2023-01-19 17:06:52 +0900

    Update copyright.js

--- js/copyright.js
+++ js/copyright.js
@@ -1,2 +1,2 @@
 document.getElementById("copyright").insertAdjacentHTML('afterbegin', '🄫2022~' + new Date().getFullYear() + '　Co+work21(2022)');
-let copyright = '🄫2022~' + new Date().getFullYear() + '　Co+work21(2022)';
+let years = '🄫2022~' + new Date().getFullYear() + '　Co+work21(2022)';
