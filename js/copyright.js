ecc91ea69309c46013ebe449b2001afe33f65576
SplaIka <107166432+splaika135@users.noreply.github.com>
2023-01-19 16:19:57 +0900

    Update copyright.js

--- js/copyright.js
+++ js/copyright.js
@@ -1,1 +1,2 @@
 document.getElementById("copyright").insertAdjacentHTML('afterbegin', '🄫2022~' + new Date().getFullYear() + '　Co+work21(2022)');
+let copyright = '🄫2022~' + new Date().getFullYear() + '　Co+work21(2022)';
