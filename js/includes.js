fa1f1dae1929c9f27237b0d1151923247488f177
SplaIka <107166432+splaika135@users.noreply.github.com>
2023-02-02 13:39:09 +0900

    Update includes.js

--- js/includes.js
+++ js/includes.js
@@ -4,9 +4,9 @@ window.onload = function(){
 
     // head
     const head_content = `
-    <link rel="stylesheet" href="style/reset.min.css">
-    <link rel="stylesheet" type="text/css" href="style/style.css">
-    <link rel="stylesheet" href="style/header-3.css">
+    <link rel="stylesheet" href="/NITAC_map/style/reset.min.css">
+    <link rel="stylesheet" type="text/css" href="/NITAC_map/style/style.css">
+    <link rel="stylesheet" href="/NITAC_map/style/header-3.css">
     `;
 
     const head = document.getElementById('head'); // 追加する部分を取得
@@ -24,18 +24,18 @@ window.onload = function(){
       <div class="site-header__end">
         <nav class="nav">
           <button class="nav__toggle" aria-expanded="false" type="button">
-            <img src="images/menu.png" width="30" height="28">
+            <img src="/NITAC_map/images/menu.png" width="30" height="28">
           </button>
           <ul class="nav__wrapper">
-            <li class="nav__item"><a href="map/G.html">G科棟</a></li>
-            <li class="nav__item"><a href="map/M.html">M科棟</a></li>
-            <li class="nav__item"><a href="map/E.html">E科棟</a></li>
-            <li class="nav__item"><a href="map/C.html">C科棟</a></li>
-            <li class="nav__item"><a href="map/A.html">A科棟</a></li>
-            <li class="nav__item"><a href="map/lib.html">情報センター</a></li>
-            <li class="nav__item"><a href="map/eat.html">食堂</a></li>
-            <!--  <li class="nav__item"><a href="route/route.html">ルート案内</a></li>  -->
-            <li class="nav__item"><a href="images/images.html">マップ画像集</a></li>
+            <li class="nav__item"><a href="/NITAC_map/map/G.html">G科棟</a></li>
+            <li class="nav__item"><a href="/NITAC_map/map/M.html">M科棟</a></li>
+            <li class="nav__item"><a href="/NITAC_map/map/E.html">E科棟</a></li>
+            <li class="nav__item"><a href="/NITAC_map/map/C.html">C科棟</a></li>
+            <li class="nav__item"><a href="/NITAC_map/map/A.html">A科棟</a></li>
+            <li class="nav__item"><a href="/NITAC_map/map/lib.html">情報センター</a></li>
+            <li class="nav__item"><a href="/NITAC_map/map/eat.html">食堂</a></li>
+            <!--  <li class="nav__item"><a href="/NITAC_map/route/route.html">ルート案内</a></li>  -->
+            <li class="nav__item"><a href="/NITAC_map/images/images.html">マップ画像集</a></li>
           </ul>
         </nav>
       </div>
