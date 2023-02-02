c007284dab15660bc097c4617ae8ab4691f8ae70
SplaIka <107166432+splaika135@users.noreply.github.com>
2023-02-02 13:15:47 +0900

    Update includes.js

--- js/includes.js
+++ js/includes.js
@@ -47,6 +47,23 @@ window.onload = function(){
     header.insertAdjacentHTML('afterbegin', header_content); // insertAdjacentHTMLでHTMLを追加
 
 
+    // header-3.js
+    let navToggle = document.querySelector(".nav__toggle");
+    let navWrapper = document.querySelector(".nav__wrapper");
+
+    navToggle.addEventListener("click", function () {
+        if (navWrapper.classList.contains("active")) {
+            this.setAttribute("aria-expanded", "false");
+            this.setAttribute("aria-label", "menu");
+            navWrapper.classList.remove("active");
+        } else {
+            navWrapper.classList.add("active");
+            this.setAttribute("aria-label", "close menu");
+            this.setAttribute("aria-expanded", "true");
+        }
+    });
+
+
 
     // navfooter
     const navfooter_content = `<ul class="navfooter__wrapper">
